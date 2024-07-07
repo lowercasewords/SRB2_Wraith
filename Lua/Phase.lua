b9b29199be593465ea17a6faa2eefdf1e0ae8e37
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2024-07-06 19:20:14 -0700

    better triggering and now we can detect when exiting phase state

--- Lua/Phase.lua
+++ Lua/Phase.lua
@@ -1,8 +1,25 @@
+-- 
+-- 		Creates two circles which are kept around the player during the phase state
+--
+-- 	The first circle is small created by the lantern, it damages any enemy or monitor on contact
+-- 
+-- 	The second circle is larger and created by darkness. If enemy or monitor gets into this area, a tenticle would spawn at 
+-- player's location that would grab an snatch the enemy towards the player.
 
 freeslot("S_PHASE")
 
 
-local LEVITATION_MOMZ = -2*FRACUNIT
+
+local PHASE_GRAB_FLAGS = MF_ENEMY|MF_MONITOR
+local PHASE_DAMAGE_FLAGS = MF_SHOOTABLE
+
+local LEVITATION_MOMZ = 2*FRACUNIT
+local PHASE_ACCELERATION = 10*FRACUNIT
+
+local DARKNESS_DISTANCE_MAX = 300*FRACUNIT
+local DARKNESS_DISTANCE_MIN = 75*FRACUNIT
+local LIGHT_DISTANCE_MAX = DARKNESS_DISTANCE_MIN
+
 --[[
 local function A_OnLevitation(playmo)
 	playmo.can_levitate = false
@@ -13,11 +30,12 @@ end
 --Levitation is used continuously while holding jump button in the air
 states[S_PHASE] = {
 	sprite = SPR_PLAY,
-	frame = SPR2_RUN_|FF_TRANS50,
+	frame = SPR2_RUN_|FF_FULLBRIGHT|FF_ADD|FF_ANIMATE,
 	-- action = A_OnLevitation,
-	tics = -1,
+	tics = 9999,
 	nextstate = S_PLAY_FALL
 }
+
 
 
 --[[
@@ -26,12 +44,132 @@ states[S_PHASE] = {
 ]]--
 
 
+
+--Tries to find a target in the fixed_t radius around the player
+local function FindTarget(player, min_distance, max_distance) 
+	if(player.valid == true and player.mo.valid == true) then
+	
+		local enemy = nil
+
+		searchBlockmap("objects", function(playmo, foundmo)
+			
+			local found_distance = R_PointToDist2(playmo.x, playmo.y, foundmo.x, foundmo.y)
+
+			if(min_distance <= found_distance and found_distance <= max_distance) then
+				enemy = foundmo
+				return false 
+			end
+		
+		end, player.mo, player.mo.x+max_distance*5000, player.mo.x-max_distance*5000, player.mo.y+max_distance*5000, player.mo.y-max_distance*5000)
+		
+		return enemy
+	end
+end
+
+addHook("PlayerSpawn", 
+	function(player)
+		player.mo.prevstate = player.mo.state
+	end)
+
+addHook("PlayerThink", 
+	function(player)
+		if(player.valid == true and player.mo.valid == true) then
+
+			
+					--Current order in a single tic
+				--Going into phase
+				--Changing from phase state to any other
+				--While in the phase
+				--Detecting when just entered the phase (doesn't work if state tics is -1)
+				--Getting out of the phase state prematurely if let go of the spin button
+
+					--Better order
+				--Getting out of the phase state prematurely if let go of the spin button
+				--Changing from phase state to any other
+				--Going into phase
+				--Detecting when just entered the phase (doesn't work if state tics is -1)
+				--While in the phase
+
+
+				-- print("	   before prev:"..player.mo.prevstate)
+				-- print("	   before state:"..player.mo.state)	
+
+
+				--Getting out of the phase state prematurely if let go of the spin button
+				if(player.mo.state == S_PHASE and player.spinheld == 0) then
+					player.mo.state = states[player.mo.state].nextstate
+				end
+
+				--Changing from phase state to any other
+				if(player.mo.prevstate == S_PHASE and player.mo.prevstate ~= player.mo.state) then
+					
+					print("stop levitation")
+					player.acceleration = skins[player.mo.skin].acceleration
+					
+					--[[
+					player.mo.color = skins[player.mo.skin].prefcolor
+					player.mo.renderflags = $ &~RF_FULLBRIGHT &~RF_NOCOLORMAPS
+					player.mo.blendmode = AST_COPY
+					]]--
+				end
+
+				--Going into phase
+				if(player.mo.state ~= S_PHASE and player.spinheld >= 1 and player.mo.state ~= player.mo.info.painstate and player.mo.state ~= player.mo.info.deathstate) then 
+					print("to phase from "..player.mo.state)
+					player.mo.state = S_PHASE
+				end
+
+				--Detecting when just entered the phase (doesn't work if state tics is -1)
+				if(player.mo.state == S_PHASE and states[player.mo.state].tics == player.mo.tics) then
+					print("on changed")
+					player.acceleration = $*3--PHASE_ACCELERATION
+				end
+
+
+				--While in the phase
+				if(player.mo.state == S_PHASE) then
+					
+					--Upper momentum
+					if(player.mo.momz <= 5*FRACUNIT and player.jumpheld >= 1) then
+						-- player.mo.momz = $+FRACUNIT
+						P_SetObjectMomZ(player.mo, 2*LEVITATION_MOMZ, false)
+					--Downwards momentum
+					elseif(not P_IsObjectOnGround(player.mo)) then--if(player.mo.momz < 0) then
+						P_SetObjectMomZ(player.mo, -LEVITATION_MOMZ, false)
+					end
+					
+			
+					--Try to find an enemy
+					local enemy = FindTarget(player, DARKNESS_DISTANCE_MIN, DARKNESS_DISTANCE_MAX)
+
+					--If an enemy is found, grab it
+					if(enemy ~= nil and enemy.flags & PHASE_GRAB_FLAGS) then
+						P_InstaThrust(enemy, R_PointToAngle2(enemy.x, enemy.y, player.mo.x, player.mo.y), player.speed/2 + 20*FRACUNIT)
+					end
+
+					--Tries to find an enemy
+					enemy = FindTarget(player, 0, LIGHT_DISTANCE_MAX)
+					--If an enemy is found, damage it
+					if(enemy ~= nil and enemy.flags & PHASE_DAMAGE_FLAGS) then
+						P_DamageMobj(enemy, player.mo, player.mo)
+					end
+			
+				end
+
+
+				-- print("		after prev:"..player.mo.prevstate)
+				-- print("		after state:"..player.mo.state)			
+				
+				player.mo.prevstate = player.mo.state 
+		end
+	end)
+
+
 addHook("PreThinkFrame", 
 	function()
 		for player in players.iterate() do
 			if(player.mo.skin == "wraith") then
-
-
+				
 				--Record holding spin
 				if(player.cmd.buttons & BT_SPIN) then
 					player.spinheld = $+1
@@ -47,61 +185,43 @@ addHook("PreThinkFrame",
 				end
 
 
+				
 
-
-				--[[
-				if(player.mo.state ~= S_PHASE) then --If not in phase state yet...
-					
-					--Phasing..
-					if(player.spinheld >= 1) then 
-						player.mo.state = S_PHASE
-					end
-					-- print("switching...")
-				end
-				]]--
 			end
 		end
 	end
 )
 
+
+--[[
+addHook("PlayerThink", 
+	function(player) 
+		if(player.valid == true and player.mo.skin == "wraith") then
+			FindTarget(player, DARKNESS_RADIUS)
+		end
+	end)
+]]--
+
+
 addHook("PostThinkFrame", 
 	function()
 		for player in players.iterate() do
 			if(player.mo.skin == "wraith") then
-
-				--If not in phase state yet...
-				if(player.mo.state ~= S_PHASE) then 
-					
-					--Phasing...
-					if(player.spinheld >= 1 and player.mo.state ~= player.mo.info.painstate and player.mo.state ~= player.mo.info.deathstate) then 
-						player.mo.state = S_PHASE
-					end
-					
-				end
-			end
-			if(player.mo.state == S_PHASE) then  
-
-				--Levitation
-				if(not P_IsObjectOnGround(player.mo)) then
-					player.mo.momz = LEVITATION_MOMZ
-				end
-
-				--Getting out of the phase state prematurely if let go of the spin button
-				if(player.spinheld == 0) then
-					player.mo.state = states[player.mo.state].nextstate
-				end
-			end
+				
+			end	
 		end 
 	end)
 
+
+
+--[[
 --Levitation script
 addHook("PlayerThink", 
 	function(player)
 		if(player.valid == true and player.mo.valid == true) then
 			
 			
-
-			--[[
+			
 			if(player.mo.state ~= S_LEVITATION) then
 				--Levitation is allowed on the next jump press in the air
 				if(player.spinheld == 0 and player.lastbuttons & BT_JUMP and not P_IsObjectOnGround(player.mo)) then
@@ -134,7 +254,7 @@ addHook("PlayerThink",
 					player.mo.momz = LEVITATION_MOMZ
 				end
 			end
-			]]--
 		end
 	end
 )
+]]--
